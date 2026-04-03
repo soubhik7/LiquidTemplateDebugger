e569f0584cdb6bc700acef401572e15f4da52a78
Soubhik Mukhopadhyay <Soubhik.mukhopadhyay@effem.com>
2026-04-03 23:57:04 +0530

    add14

--- Program.cs
+++ Program.cs
@@ -21,11 +21,6 @@ if (args.Length == 1 && args[0] == "--formattest")
 {
     return FormatTransformationTest.Run();
 }
-if (args.Length == 1 && args[0] == "--soldtopartytest")
-{
-    return SoldToPartyFeatureTest.Run();
-}
-
 // Parse port from args
 var port = 5050;
 string? preloadTemplate = null;
@@ -52,7 +47,6 @@ for (int i = 0; i < args.Length; i++)
         Console.WriteLine("  --bugtest              Run bug reproduction tests and exit");
         Console.WriteLine("  --xmltest              Run complex XML to XML transformation test and exit");
         Console.WriteLine("  --formattest           Run format transformation tests and exit");
-        Console.WriteLine("  --soldtopartytest      Run soldToParty.location feature test and exit");
         Console.WriteLine("  --help, -h             Show this help");
         Console.WriteLine();
         Console.WriteLine("If template and data files are provided, they are pre-loaded into the session.");
